4028de50fe2229fcd26ab3af4631b982dc9dc335
sunildevops45 <sunildevops45@gmail.com>
2026-01-28 01:14:10 +0530

    chsges

--- bookstore-frontend-react-app/src/constants/appConstants.js
+++ bookstore-frontend-react-app/src/constants/appConstants.js
@@ -1,3 +1,3 @@
-export const BACKEND_API_GATEWAY_URL = 'http://api.rajureddy.live';
+export const BACKEND_API_GATEWAY_URL = 'http://152.42.156.116';
 export const APP_CLIENT_ID = '93ed453e-b7ac-4192-a6d4-c45fae0d99ac';
 export const APP_CLIENT_SECRET = 'client.devd123';
