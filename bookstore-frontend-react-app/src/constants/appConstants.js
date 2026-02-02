c6cc9957e4d978e9af7be69cf4cb0782173e5fe1
sunildevops45 <sunildevops45@gmail.com>
2026-02-02 22:41:50 +0530

    Update appConstants.js

--- bookstore-frontend-react-app/src/constants/appConstants.js
+++ bookstore-frontend-react-app/src/constants/appConstants.js
@@ -1,3 +1,3 @@
-export const BACKEND_API_GATEWAY_URL = 'http://api.rajureddy.liv';
+export const BACKEND_API_GATEWAY_URL = 'http://api.sunilreddy.shop';
 export const APP_CLIENT_ID = '93ed453e-b7ac-4192-a6d4-c45fae0d99ac';
 export const APP_CLIENT_SECRET = 'client.devd123';
